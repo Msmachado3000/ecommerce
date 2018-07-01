444a26743951326b3554b29eacac99c4e8ce157d
Marcelo Machado <marcelo.update.msm@gmail.com>
2018-07-01 19:06:08 -0300

    Minha Conta

--- site.php
+++ site.php
@@ -317,4 +317,72 @@ $app->post("/forgot/reset", function(){
 
 });
 
+$app->get("/profile", function(){
+
+	User::verifyLogin(false);
+
+	$user = User::getFromSession();
+
+	$page = new Page();
+
+	$page->setTpl("profile", [
+		'user'=>$user->getValues(),
+		'profileMsg'=>User::getSuccess(),
+		'profileError'=>User::getError()
+	]);
+
+});
+
+$app->post("/profile", function(){
+
+	User::verifyLogin(false);
+
+	if (!isset($_POST['desperson']) || $_POST['desperson'] ==='') {
+		User::setErro("Preencha o seu nome.");
+		header("Location: /profile");
+		exit;
+	}
+
+	if (!isset($_POST['desemail']) || $_POST['desemail'] ==='') {
+		User::setErro("Preencha o seu e-mail.");
+		header("Location: /profile");
+		exit;
+	}
+
+	if (!isset($_POST['nrphone']) || $_POST['nrphone'] ==='') {
+		User::setErro("Preencha o seu Telefone.");
+		header("Location: /profile");
+		exit;
+	}
+
+	$user = User::getFromSession();
+
+	if ($_POST['desemail'] !== $user->getdesemail()){
+
+		if (User::checkLoginExist($_POST['desemail']) === true){
+
+			User::setError("Este endereço de e-mail ja está cadastrado.");
+			header("Location: /profile");
+			exit;
+
+		}
+
+	}
+
+	$user->setData($_POST);
+
+	//$_POST['desperson'] = $user->getdesperson();
+	$_POST['inadmin'] = $user->getinadmin();
+	$_POST['despassword'] = $user->getdespassword();
+	$_POST['deslogin'] = $_POST['desemail'];
+
+	$user->update();
+	$_SESSION[User::SESSION] = $user->getValues();
+	User::setSuccess("Dados alterados com sucesso!!");
+
+	header("Location: /profile");
+	exit;
+
+})
+
 ?>
