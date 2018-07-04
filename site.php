62a9d3a74cfbad1391d10989e72264a495f85623
Marcelo Machado <marcelo.update.msm@gmail.com>
2018-07-04 09:31:19 -0300

    Problema ao salvar Address

--- site.php
+++ site.php
@@ -6,6 +6,8 @@ use \Hcode\Model\Category;
 use \Hcode\Model\Cart;
 use \Hcode\Model\Address;
 use \Hcode\Model\User;
+use \Hcode\Model\Order;
+use \Hcode\Model\OrderStatus;
 
 $app->get('/', function() {
 
@@ -234,7 +236,23 @@ $app->post("/checkout", function(){
 
 	$address->save();
 
-	header("Location: /order");
+	$cart = Cart::getFromSession();
+
+	$totals = $cart->getCalculateTotal();
+
+	$order = new Order();
+
+	$order->setData([
+		'idcart'=>$cart->getidcart(),
+		'idaddress'=>$address->getidaddress(),
+		'iduser'=>$user->getiduser(),
+		'idstatus'=>OrderStatus::EM_ABERTO,
+		'vltotal'=>$totals['vlprice'] + $cart->getvlfreight()
+	]);
+
+	$order->save();
+
+	header("Location: /order/" . $cart->getidorder());
 	exit;
 
 });
@@ -451,6 +469,24 @@ $app->post("/profile", function(){
 	header('Location: /profile');
 	exit;
 
+	});
+
+$app->get("/order/:idorder", function($idorder) {
+
+	User::verifyLogin(false);
+
+	$order = new Order();
+
+	$order->get((int)$idorder);
+
+	$page = new Page();
+
+	$page->setTpl("payment", [
+		'order'=>$order->getValues()
+	]);
+
 });
 
- ?>
+
+
+?>
